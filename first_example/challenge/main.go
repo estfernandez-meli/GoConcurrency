bfd655303b14f7ae0c4e1c18b19b6f3e50add600
estfernandez_meli <estiben.fernandez@mercadolibre.com.co>
2025-08-13 08:46:09 -0500

    refactor/replace manual implementation by an for

--- first_example/challenge/main.go
+++ first_example/challenge/main.go
@@ -17,21 +17,34 @@ func printMessage() {
 }
 func main() {
 	var wg sync.WaitGroup
-	msg = "Hello, world"
+	//msg = "Hello, world"
+	//
+	//wg.Add(1)
+	//go updateMessage("universe", &wg)
+	//wg.Wait()
+	//printMessage()
+	//
+	//wg.Add(1)
+	//go updateMessage("cosmos", &wg)
+	//wg.Wait()
+	//printMessage()
 
-	wg.Add(1)
-	go updateMessage("universe", &wg)
-	wg.Wait()
-	printMessage()
+	//wg.Add(1)
+	//go updateMessage("world", &wg)
+	//wg.Wait()
+	//printMessage()
 
-	wg.Add(1)
-	go updateMessage("cosmos", &wg)
-	wg.Wait()
-	printMessage()
+	words := []string{
+		"universe",
+		"cosmos",
+		"world",
+	}
 
-	wg.Add(1)
-	go updateMessage("world", &wg)
-	wg.Wait()
-	printMessage()
+	for _, w := range words {
+		wg.Add(1)
+		go updateMessage(w, &wg)
+		wg.Wait()
+		printMessage()
+	}
 
 }
