9546e76852b2de3ea1848768b130b8d9a3926845
estfernandez_meli <estiben.fernandez@mercadolibre.com.co>
2025-08-14 08:43:42 -0500

    implement mutex to avoid race conditions

--- example2/main.go
+++ example2/main.go
@@ -8,17 +8,19 @@ import (
 var msg string
 var wg sync.WaitGroup
 
-func updateMsg(s string) {
+func updateMsg(s string, m *sync.Mutex) {
 	defer wg.Done()
+	m.Lock()
 	msg = fmt.Sprintf("Hello, %s", s)
+	m.Unlock()
 }
 
 func main() {
 	msg = "Hello, world!"
-
+	var mutex sync.Mutex
 	wg.Add(2)
-	go updateMsg("universe")
-	go updateMsg("cosmos")
+	go updateMsg("universe", &mutex)
+	go updateMsg("cosmos", &mutex)
 
 	wg.Wait()
 
