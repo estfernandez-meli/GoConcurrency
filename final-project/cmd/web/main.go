85f29a338f9e8065069d16d49c68eda9ae0c002c
estfernandez_meli <estiben.fernandez@mercadolibre.com.co>
2025-11-07 08:03:40 -0500

    Initialize first project

---
+++ final-project/cmd/web/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+	//Connect to the database
+
+	// create sessions
+
+	// create channels
+
+	// create wait group
+
+	// set up the application config
+
+	// set up mail
+
+	//listen for web connections
+}
