7e80cab3a6f93d8ce22da27fe8a9d74d3af96847
estfernandez_meli <estiben.fernandez@mercadolibre.com.co>
2025-11-17 14:32:45 -0500

    feat:db and reddis configuration

--- final-project/cmd/web/main.go
+++ final-project/cmd/web/main.go
@@ -1,9 +1,27 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/alexedwards/scs/redisstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/gomodule/redigo/redis"
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+const webPort = "80"
 func main() {
 	//Connect to the database
+	db := initDB()
+	db.Ping()
 
 	// create sessions
+	session := initSession()
 
 	// create channels
 
@@ -14,4 +32,68 @@ func main() {
 	// set up mail
 
 	//listen for web connections
+}
+
+func initDB() *sql.DB {
+	conn := connectDB()
+	if conn == nil {
+		log.Panic("cant connect to database")
+	}
+	return conn
+}
+
+func connectDB() *sql.DB {
+	counts := 0
+	dsn := os.Getenv("DSN")
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("postgresSQL not ready yet...")
+		} else {
+			log.Println("connected to database")
+			return connection
+		}
+		if counts > 10 {
+			return nil
+		}
+
+		log.Print("Backing off for 1 second")
+		time.Sleep(1 * time.Second)
+		counts +=1
+		continue
+
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error){
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func initSession() *scs.SessionManager {
+	//set up session
+	session := scs.New()
+	session.Store = redisstore.New(initRedis())
+	session.Lifetime = 24  * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+	return session
+}
+
+func initRedis() *redis.Pool {
+	redisPool := &redis.Pool{
+		MaxIdle: 10,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", os.Getenv("REDIS"))
+		},
+	}
+	return redisPool
 }
